8e78147455f1ba380ae670863f72b71ed03d9bda
Martin Kirilov <wucdbm@gmail.com>
2018-01-31 03:34:08 +0200

    add secret

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -1,29 +1,75 @@
 <?php
 
-/*
- * This file is part of the ForciMenuBuilderApiBundle package.
- *
- * Copyright (c) Forci Web Consulting Ltd.
- *
- * Author Martin Kirilov <martin@forci.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-namespace Forci\Bundle\MenuBuilderApi\Controller;
+namespace Forci\Bundle\MenuBuilderApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Forci\Bundle\MenuBuilderBundle\Entity\Menu;
 use Forci\Bundle\MenuBuilderBundle\Entity\MenuItem;
 
 class MenuController extends Controller {
 
-    public function getAction($id) {
+    public function multiGetAction(Request $request) {
+        if ($request->query->get('secret') != $this->container->getParameter('forci_menu_builder_api.secret')) {
+            return $this->json([]);
+        }
+
+        $ids = $request->query->get('ids');
+
+        if (!is_array($ids)) {
+            return $this->json([]);
+        }
+
         $manager = $this->container->get('forci_menu_builder.manager.menus');
+
+        $data = [];
+
+        foreach ($ids as $id) {
+            /** @var Menu $menu */
+            $menu = $manager->findOneById($id);
+
+            if (!$menu) {
+                $data[$id] = [];
+                continue;
+            }
+
+            if (!$menu->getIsApiVisible()) {
+                $data[$id] = [];
+                continue;
+            }
+
+            $data[$id] = $this->fetchMenu($menu);
+        }
+
+        return $this->json($data);
+    }
+
+    public function getAction($id, Request $request) {
+        if ($request->query->get('secret') != $this->container->getParameter('forci_menu_builder_api.secret')) {
+            return $this->json([]);
+        }
+
+        $manager = $this->container->get('forci_menu_builder.manager.menus');
+
         $menu = $manager->findOneById($id);
 
+        if (!$menu) {
+            return $this->json([]);
+        }
+
+        if (!$menu->getIsApiVisible()) {
+            return $this->json([]);
+        }
+
+        $data = $this->fetchMenu($menu);
+
+        return $this->json($data);
+    }
+
+    protected function fetchMenu(Menu $menu) {
         $data = [
-            'name' => $menu->getName()
+            'name'     => $menu->getName(),
+            'modified' => $menu->getDateModified()->format('U')
         ];
 
         /** @var MenuItem $item */
@@ -34,14 +80,14 @@ class MenuController extends Controller {
             $data['items'][] = $this->fetchItem($item);
         }
 
-        return $this->json($data);
+        return $data;
     }
 
     protected function fetchItem(MenuItem $item) {
         $manager = $this->container->get('forci_menu_builder.manager.menus');
         $data = [
-            'name' => $item->getName(),
-            'url' => $manager->generateMenuItemUrl($item),
+            'name'     => $item->getName(),
+            'url'      => $manager->generateMenuItemUrl($item),
             'children' => []
         ];
         foreach ($item->getChildren() as $child) {
@@ -50,4 +96,5 @@ class MenuController extends Controller {
 
         return $data;
     }
+
 }
---
+++ src/DependencyInjection/ForciMenuBuilderApiExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Forci\Bundle\MenuBuilderApiBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class ForciMenuBuilderApiExtension extends Extension {
+
+    public function load(array $configs, ContainerBuilder $container) {
+        $configuration = $this->getConfiguration($configs, $container);
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $bag = $container->getParameterBag();
+
+        $bag->set('forci_menu_builder_api.config', $config);
+
+        $bag->set('forci_menu_builder_api.secret', $config['secret']);
+    }
+
+    public function getXsdValidationBasePath() {
+        return __DIR__ . '/../Resources/config/';
+    }
+
+    public function getNamespace() {
+        return 'http://www.example.com/symfony/schema/';
+    }
+
+}
